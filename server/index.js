2a5ca5c13d1bf838d2800957cf88e87022485550
Veronica <weronika.bakanowicz@g.austincc.edu>
2020-07-20 20:58:30 -0500

    solved first 2 steps

---
+++ server/index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express"),
+      session = require("express-session"),
+      massive = require("massive");
+
+
+      const app = express();
+
+      app.use(express.json());
+
+      const PORT = 4000;
+
+      let {CONNECTION_STRING, SESSION_SECRET} = process.env;
+
+      massive({
+          connectionString: CONNECTION_STRING,
+          ssl: {
+              rejectUnauthorized: false,
+          },
+      }).then((db) => {
+          app.set("db", db)
+          console.log("db connected")
+      })
+
+      app.use(
+          session({
+              secret: SESSION_SECRET,
+              resave: true,
+              saveUninitialized: false,
+            //   cookie:{ maxAge: 1000 * 60 * 60 * 24 },
+          })
+      );
+
+      app.listen(PORT, () => {
+          console.log(`Server listening on port: ${PORT}`)
+      })
